28d3e94e26a19c27db80c1daa74e8a28dabb1d5e
daeuun <leeda51925@gmail.com>
2023-03-06 01:03:31 +0900

    landing page

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.thmoviedb.org/3';
+export const API_KEY = '76c4d5f9d525670c7b300a06762ee690';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
